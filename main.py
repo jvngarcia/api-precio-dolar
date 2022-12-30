21deb117891d837abf1e0d339529fa3499c6f487
Angel Garcia <angelgarciaweb@gmail.com>
2022-12-30 11:31:42 -0400

    Precio de dolar paralelo y BCV

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from fastapi import FastAPI
-import pandas as pd
+from fastapi import FastAPI, HTTPException
+from bs4 import BeautifulSoup
 import requests
+import json
 
 app = FastAPI()
 
 @app.get('/')
 def home( ):
+    URL = "https://monitordolarvenezuela.com/"
+    URL_BCV = 'https://www.bcv.org.ve/'
 
-    return { 'Hello' : 'world' }
+    req = requests.get(URL)
+
+    status_code = req.status_code
+
+    if status_code != 200:
+        raise HTTPException( status_code = 404, detail = "Sitio web no encontrado" )
+
+    soup = BeautifulSoup(req.text, 'lxml')
+
+    contenedor_promedios = soup.find_all('div', class_="col-12 col-sm-4 col-md-2 col-lg-2")
+
+    textos = {}
+
+    for promedios in contenedor_promedios:
+        textos[promedios.h4.string] = promedios.p.string
+
+    bcv = textos['BCV (Oficial)'].lstrip('Bs = ')
+    paralelo = textos['@EnParaleloVzla3'].lstrip('Bs = ')
+
+    return {
+        'BCV' : bcv,
+        'En Paralelo' : paralelo
+    }   
