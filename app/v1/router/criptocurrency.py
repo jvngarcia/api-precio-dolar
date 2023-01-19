8da7fec05c1a1b13713575c1d4f42c5ec7ee241f
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-19 10:04:52 -0400

    refactor: Actualizando nombre de archivos

---
+++ app/v1/router/criptocurrency.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter
+import pandas as pd
+
+
+
+router = APIRouter( prefix = "/api/v1/criptocurrency" )
+
+
+class price_criptocurrency:
+    @router.get("/btc")
+    def get_btc():
+
+        URL = "https://coinmarketcap.com/"
+        
+        dfs = pd.read_html(URL)
+
+        # Obtener la primera tabla
+        df = dfs[0]
+
+        # Extraer las columnas
+        df2 = df[['Name','Price']]
+
+        nombre = df2['Name'].iloc[0].rstrip('1BTC')
+        precio = df2['Price'].iloc[0].lstrip('$')
+        moneda = df2['Price'].iloc[0][0:1]
+
+        return {
+            'name' : nombre,
+            'price' : precio,
+            'currency' : moneda,
+        }
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 
 # Importa las rutas del aplicativo
 from app.v1.router import changing_table
-from app.v1.router import price_criptocurrency
+from app.v1.router import criptocurrency
 
 app = FastAPI()
 app.include_router( changing_table.router )
-app.include_router( price_criptocurrency.router )
+app.include_router( criptocurrency.router )
